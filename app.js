3ff3d5bb0624d70c72f93d3fc5c338a93660676d
darkmatter <darkmatter8128@gmail.com>
2025-01-26 22:33:21 +0900

    upgrade

--- app.js
+++ app.js
@@ -1,31 +1,23 @@
-const kv = await Deno.openKv();
-await kv.set(["users"], {});
-
-const setClients = async (socket) => {
-    let users = await kv.get(["users"]);
-    users[JSON.stringify(socket)] = "";
-    await kv.set(["users"], {users});
-};
-
 Deno.serve({ port: 3000 }, (req) => {
-    if (req.headers.get("upgrade") !== "websocket") {
-        return new Response(null, { status: 501 });
+    console.log(req.url);
+
+    if (req.url === "http://localhost:3000/") {
+        if (req.headers.get("upgrade") !== "websocket") {
+            return new Response(null, { status: 501 });
+        }
+        const { socket, response } = Deno.upgradeWebSocket(req);
+        socket.addEventListener("open", async () => {
+            console.log("a client connected!");
+        });
+
+        socket.onclose = async () => {
+        };
+
+        socket.addEventListener("message", async (event) => {
+            console.log(event.data);
+
+            socket.send("pong");
+        });
+        return response;
     }
-    const { socket, response } = Deno.upgradeWebSocket(req);
-    socket.addEventListener("open", async () => {
-        console.log("a client connected!");
-        setClients(socket);
-        console.log(await kv.get(["users"]));
-    });
-
-    socket.onclose = async () => {
-
-    };
-
-    socket.addEventListener("message", async (event) => {
-        console.log(event.data);
-
-        socket.send("pong");
-    });
-    return response;
 });
